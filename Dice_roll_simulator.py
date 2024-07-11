f489ba48ecb548cdda0e338913b3adbd1d6573c2
bigABG <abdelaziz.benghalia@gmail.com>
2024-07-12 00:23:08 +0100

    Added conditions for the num and sides

--- Dice_roll_simulator.py
+++ Dice_roll_simulator.py
@@ -9,9 +9,15 @@ def rolls(num, roll, sides):
 
 if __name__ == "__main__":
     num = int(input("number of dice rolls : "))  # Asking user for the number of dice rolls
-    
+    while num <= 0:
+        print("Number of rolls must be a positive integer.")
+        num = int(input("number of dice rolls : "))
+
     sides = int(input("number of sides on the dice : "))  # Asking user for the number of sides on the dice
-    
-    roll = array([int()] * num)  # Initializing an array of zeros with length 'num'
+    while sides <= 0:
+        print("Number of sides must be a positive integer.")
+        sides = int(input("number of sides on the dice : "))
+
+    roll = array([0] * num)  # Initializing an array of zeros with length 'num'
     
     print(rolls(num, roll, sides))  # Printing the result of the rolls function
